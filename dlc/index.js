75881b20dbd8215e770be9456bf146ee38aa1dbe
SpaceWhisky <tudor.paisa@gmail.com>
2025-05-27 18:56:09 +0200

    add test dlc

--- dlc/index.js
+++ dlc/index.js
@@ -1,5 +1,14 @@
 [
     { 
+        "file": "test.dlc", 
+        "title": "Test",
+        "buttons": [
+            { "title": "Test 1", "action": [75,0,0,3] },
+            { "title": "Test 2", "action": [75,0,4,4] },
+            { "title": "Test 3", "action": [75,0,3,4] }
+        ]
+    },
+    { 
         "file": "context.dlc", 
         "title": "Context Logo",
         "buttons": [
