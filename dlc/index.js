fba3622b06ae873c66dde0d4c89c8ca54c1ecfee
SpaceWhisky <tudor.paisa@gmail.com>
2025-05-27 20:22:54 +0200

    update dlc

--- dlc/index.js
+++ dlc/index.js
@@ -1,7 +1,16 @@
 [
     { 
-        "file": "test.dlc", 
-        "title": "Test",
+        "file": "test1.dlc", 
+        "title": "Test1",
+        "buttons": [
+            { "title": "Test 1", "action": [75,0,0,3] },
+            { "title": "Test 2", "action": [75,0,4,4] },
+            { "title": "Test 3", "action": [75,0,3,4] }
+        ]
+    },
+    { 
+        "file": "test2.dlc", 
+        "title": "Test2",
         "buttons": [
             { "title": "Test 1", "action": [75,0,0,3] },
             { "title": "Test 2", "action": [75,0,4,4] },
